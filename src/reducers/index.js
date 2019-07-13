eb42ca9e207aaa3945b5a87e3e4f147fece2ea5d
Dewinurdin <dewinurdin@gmail.com>
2019-07-13 12:39:20 -0500

    created selectedSongReducer and  export combineReducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,5 @@
+import { combineReducers } from 'redux';
+
 const songsReducer = () => {
   return [
     { title: 'No Scrubs', duration: '4:05' },
@@ -6,3 +8,21 @@ const songsReducer = () => {
     { title: 'I want it That Way', duration: '1:45' },
   ];
 };
+
+// first argument, selectedSong = null --> to indicate that our application first starts up,  
+//                                         it  won't have any selected song
+// second argument, action from Action Creator
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  // if action.type in action creator is equal to SONG_SELECTED
+  if (action.type === 'SONG_SELECTED'){
+    return action.payload;
+  }
+  return selectedSong;
+};
+
+// Export both reducers using combineReducers function from  Redux
+export default combineReducers({
+  songs: songsReducer,
+  selectedSong: selectedSongReducer
+});
